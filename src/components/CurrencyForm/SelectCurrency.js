d8307edfdf9b028210f4fe9dced68ab6a6a3ee82
powsitu <sendtopow@gmail.com>
2021-06-17 11:41:06 +0200

    selectcurrency now fetches available currencies from the api instead of using hard coded currencies

--- src/components/CurrencyForm/SelectCurrency.js
+++ src/components/CurrencyForm/SelectCurrency.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Form } from "react-bootstrap";
+import { availableCurrencies } from "../../store/currencyData/actions";
+import { selectCurrencies } from "../../store/currencyData/selectors";
 
 export default function SelectCurrency(props) {
+  const currencies = Object.keys(useSelector(selectCurrencies));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(availableCurrencies());
+  }, [dispatch]);
+
   return (
     <Form.Group>
       <Form.Label>{props.label}</Form.Label>
       <Form.Control as="select">
         <option>Choose...</option>
-        <option>EUR</option>
-        <option>USD</option>
-        <option>GBP</option>
+        {currencies.length > 0
+          ? currencies.map((currency, index) => {
+              return (
+                <option value={currency} key={index}>
+                  {currency}
+                </option>
+              );
+            })
+          : null}
       </Form.Control>
     </Form.Group>
   );
