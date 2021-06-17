3ac2744840f6cce1ad1e3c644aa7962882b33fa9
powsitu <sendtopow@gmail.com>
2021-06-17 23:47:32 +0200

    created selectors for weekly conversion and moved all chartfeeder logic into thechart component to have the historicalfetche inside a useeffect and only do the action once, resolving the double fetch bug

--- src/components/TheChart/index.js
+++ src/components/TheChart/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   ResponsiveContainer,
   AreaChart,
@@ -8,17 +8,60 @@ import {
   Tooltip,
 } from "recharts";
 import { format, parseISO } from "date-fns";
-import chartFeeder from "../chartFeeder";
 import Loading from "../Loading";
+import {
+  countWeeklyConversion,
+  selectWeeklyConversion,
+} from "../../store/chartData/selectors";
+import axios from "axios";
+import { subDays } from "date-fns";
+import { API_URL, API_KEY } from "../../constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addDay } from "../../store/chartData/actions";
+
+function lastSevenDays() {
+  const lastThirtyDays = [];
+  for (let num = 7; num >= 0; num--) {
+    lastThirtyDays.push(subDays(new Date(), num).toISOString().substr(0, 10));
+  }
+  return lastThirtyDays;
+}
+
+async function HistoricalFetcher(daysData, symbols, dispatch) {
+  for (let i = 0; i < daysData.length; i++) {
+    try {
+      const response = await axios.get(
+        `${API_URL}/${daysData[i]}?access_key=${API_KEY}&symbols=${symbols[0]},${symbols[1]}`
+      );
+      const dailyConversion = {
+        date: daysData[i],
+        value:
+          (1 / response.data.rates[symbols[0]]) *
+          response.data.rates[symbols[1]],
+      };
+      dispatch(addDay(dailyConversion));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+}
 
 export default function TheChart() {
-  chartFeeder();
+  const dispatch = useDispatch();
+  const dataCount = useSelector(countWeeklyConversion);
+  const data = useSelector(selectWeeklyConversion);
+
+  useEffect(() => {
+    const symbols = ["USD", "GBP"];
+    const daysData = lastSevenDays();
+    HistoricalFetcher(daysData, symbols, dispatch);
+  }, [dispatch]);
 
   return (
     <>
-      {/* {data.length !== 8 ? ( */}
-      <Loading />
-      {/* ) : (
+      {dataCount === 0 ? (
+        <Loading />
+      ) : (
         <ResponsiveContainer width="100%" height={400}>
           <AreaChart data={data}>
             <Area dataKey="value" stroke="#28a745" fill="#20c997" />
@@ -36,7 +79,7 @@ export default function TheChart() {
             <Tooltip />
           </AreaChart>
         </ResponsiveContainer>
-      )} */}
+      )}
     </>
   );
 }
