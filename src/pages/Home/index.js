ccc3724e5b74a62c74f8ff992ad3993f1e5fa2cf
powsitu <sendtopow@gmail.com>
2021-06-17 12:07:57 +0200

    added form into a bootstrap card and changed background from dark to light

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 import CurrencyForm from "../../components/CurrencyForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
     <Container>
-      <CurrencyForm />
+      <Card bg="light" text="dark">
+        <Card.Header>xChange</Card.Header>
+        <Card.Body>
+          <CurrencyForm />
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">
+            Using rates from <a href="https://ratesapi.io/">ratesapi.io</a>
+          </small>
+        </Card.Footer>
+      </Card>
     </Container>
   );
 }
