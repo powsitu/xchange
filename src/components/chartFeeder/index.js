fd156574863cfd3e5e04ab22b6c8d1af56cd301d
powsitu <sendtopow@gmail.com>
2021-06-17 20:05:37 +0200

    fetching rates for the last 30 days is working, need to combine it now

--- src/components/chartFeeder/index.js
+++ src/components/chartFeeder/index.js
@@ -1,13 +1,31 @@
+import axios from "axios";
 import { subDays } from "date-fns";
+import { API_URL, API_KEY } from "../../constants";
+
+async function historicalFetcher(daysData, symbols) {
+  for (let i = 0; i < daysData.length; i++) {
+    try {
+      const response = await axios.get(
+        `${API_URL}/${daysData[i].date}?access_key=${API_KEY}&symbols=${symbols}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+}
 
 export default function chartFeeder() {
+  const symbols = "USD,GBP";
   const lastThirtyDays = [];
   for (let num = 30; num >= 0; num--) {
     lastThirtyDays.push({
       date: subDays(new Date(), num).toISOString().substr(0, 10),
-      value: 1 + Math.random(),
+      // value: 1 + Math.random(),
     });
   }
+
+  historicalFetcher(lastThirtyDays, symbols);
 
   return lastThirtyDays;
 }
