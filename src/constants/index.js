0af21d48c361c8f7f6722628ce0754b37b4438cd
powsitu <sendtopow@gmail.com>
2021-06-17 23:50:28 +0200

    new api key

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,4 @@
 export const API_URL =
   process.env.API_URL || "http://api.exchangeratesapi.io/v1/";
 export const API_KEY =
-  process.env.API_URL || "22dfbfee04b56d6e2baee069f0cfdffd";
-
-//old key 78f4e2398bb663dde21c79b40762bccb
+  process.env.API_URL || "af305d54968975d14f756a6268d86613";
