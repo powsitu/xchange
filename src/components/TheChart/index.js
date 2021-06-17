1fca2679fae07b65ac52c928bc220a2450ac0bb2
powsitu <sendtopow@gmail.com>
2021-06-17 22:24:32 +0200

    added loading animation while fetching data for the chart

--- src/components/TheChart/index.js
+++ src/components/TheChart/index.js
@@ -9,28 +9,35 @@ import {
 } from "recharts";
 import { format, parseISO } from "date-fns";
 import chartFeeder from "../chartFeeder";
+import Loading from "../Loading";
 
 export default function TheChart() {
   const data = chartFeeder();
   console.log(data);
 
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <AreaChart data={data}>
-        <Area dataKey="value" stroke="#28a745" fill="#20c997" />
-        <XAxis
-          dataKey="date"
-          tickFormatter={(str) => {
-            const date = parseISO(str);
-            if (date.getDate() % 2 === 0) {
-              return format(date, "MMM, d");
-            }
-            return "";
-          }}
-        />
-        <YAxis dataKey="value" axisLine={false} />
-        <Tooltip />
-      </AreaChart>
-    </ResponsiveContainer>
+    <>
+      {data.length !== 8 ? (
+        <Loading />
+      ) : (
+        <ResponsiveContainer width="100%" height={400}>
+          <AreaChart data={data}>
+            <Area dataKey="value" stroke="#28a745" fill="#20c997" />
+            <XAxis
+              dataKey="date"
+              tickFormatter={(str) => {
+                const date = parseISO(str);
+                if (date.getDate() % 2 === 0) {
+                  return format(date, "MMM, d");
+                }
+                return "";
+              }}
+            />
+            <YAxis dataKey="value" axisLine={false} />
+            <Tooltip />
+          </AreaChart>
+        </ResponsiveContainer>
+      )}
+    </>
   );
 }
