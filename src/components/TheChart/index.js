0836a3c5b35c1007abcd595d1a8971d7fe32dbc9
powsitu <sendtopow@gmail.com>
2021-06-17 17:16:33 +0200

    imported recharts and created basic component to display the chart

---
+++ src/components/TheChart/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TheChart() {
+  return (
+    <div>
+      Here we gonna show a chart with the conversion value in the last 30 days!
+    </div>
+  );
+}
