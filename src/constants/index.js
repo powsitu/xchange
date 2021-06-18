0476e416dfb50297c7db1cff15a636b4dd1354c5
powsitu <60945117+powsitu@users.noreply.github.com>
2021-06-18 03:40:58 +0200

    Update index.js
    added environment variables in netlify

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const API_URL =
-  process.env.API_URL || "http://api.exchangeratesapi.io/v1/";
+  process.env.API_URL;
 export const API_KEY =
-  process.env.API_URL || "af305d54968975d14f756a6268d86613";
+  process.env.API_URL;
